8b7440e847a25a5926fc7ee2262787d03da77c22
Hamzah Khan <khanh@catlin.edu>
2013-11-06 13:15:46 -0800

    Initial Commit

---
+++ modules/db.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Team = mongoose.Schema({
+
+});
+
+var Match = mongoose.Schema({
+	
+});
+
+var Event = mongoose.Schema({
+	
+});
+
+/*
+exports.connect = function dbConnect() {
+
+};
+*/
