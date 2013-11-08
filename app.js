f47da6461a32db4adf2e0dc19598f1eb90ea1398
Hamzah Khan <khanh@catlin.edu>
2013-11-08 14:58:32 -0800

    Hook in stylus properly to the project

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
