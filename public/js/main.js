c8c64d5e4154d72708d6a1e6c1c481b8966a2346
Hamzah Khan <hmzh.khn@gmail.com>
2013-11-27 22:33:46 -0800

    Create master match control page progress bar
    Issues with the reset button: see main.js:153-155
    Does not move to verify until pause is pressed: main.js:197
    Add more checkpoints for the match

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,64 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+//returns an array of unique values
+Array.prototype.uniq = function(){
+   var u = {}, a = [];
+   for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+         continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+   }
+   return a;
+}
+
+//check that it is made of 3 teams, each 4-5 letters long and strings
+var validateTeams = function validateTeams(teamsArray) {
+  if(angular.isArray(teamsArray)) {
+    var validateAllianceCount = (teamsArray.length === 3); 
+    var validateTypes, validateWordLengths;
+
+    teamsArray.forEach(function(team) {
+      if(typeof team === 'string') {
+        validateTypes = true;
+
+        // '1540' or '1540z'
+        if(team.length <= 5 && team.length >= 1) {
+          validateWordLengths = true;
+        }
+        else {
+          validateWordLengths = false;
+        }
+      }
+      else {
+        validateTypes = false;
+      }
+    });
+
+    return (validateAllianceCount && validateTypes && validateWordLengths);
+  }
+  else {
+    return false;
+  }
+};
+
+var formatMilliseconds = function formatMillisceonds(time) {
+  if(typeof time === 'string') {
+    time = parseInt(time);
+  }
+  if(typeof time !== 'number') {
+    throw new Error('time '+ time +' is not a number');
+  }
+  var seconds = Math.floor(time/1000 % 60);
+  seconds = (seconds < 10)? '0'+seconds: seconds; 
+  var minutes = Math.floor(time/(60*1000));
+
+  //2:30
+  return minutes +':'+ seconds;
+};
 
 var app = angular.module('bunnybots2013', ['ngRoute']);
 
@@ -19,12 +77,12 @@ app.config(function ($routeProvider, $locationProvider) {
 
     when('/master', {
       templateUrl: 'partials/master_match_view',
-      controller: 'MyCtrl3'
+      controller: 'MasterMatchViewCtrl'
     }).
 
     when('/input', {
       templateUrl: 'partials/master_match_input',
-      controller: 'MyCtrl4'
+      controller: 'MasterMatchInputCtrl'
     }).
 
     when('/verify', {
@@ -54,8 +112,9 @@ app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 });
 
-app.controller('AppCtrl', function ($scope, $http) {
+app.controller('AppCtrl', function ($scope, $rootScope, $http) {
 
+  //what is this? delete?
   $http({
     method: 'GET',
     url: '/api/name'
@@ -67,6 +126,9 @@ app.controller('AppCtrl', function ($scope, $http) {
     $scope.test = 'Error!'
   });
 
+  //update by master only
+  $rootScope.currentTeams = {red:[],blue:[]};
+
 });
 app.controller('MyCtrl1', function ($scope) {
   $scope.hello = 'test';
@@ -76,14 +138,111 @@ app.controller('MyCtrl2', function ($scope) {
   $scope.hello = 'test';
 
 });
-app.controller('MyCtrl3', function ($scope) {
-  $scope.hello = 'test';
+app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location) {
 
-});
-app.controller('MyCtrl4', function ($scope) {
-  $scope.hello = 'test';
+  $scope.redTeams = $rootScope.currentTeams.red;
+  $scope.blueTeams = $rootScope.currentTeams.blue;
 
+  $scope.startMatch = function() {
+    if(!$scope.matchRunning) {
+      requestAnimationFrame($scope.bar);
+    }
+  };
+  /*
+    reset: coded for in HTML
+      ISSUE: when match is restarted, it goes back to where it was
+        after match is over reset, it just doesn't work period
+    pause: coded for in HTML
+  */
+
+  var MATCH_LENGTH = 150000;
+
+  var start;
+  var percentCompleted = 0;
+  $scope.barColorClass = "progress-bar-success";
+
+  $scope.bar = function bar(time) {
+    start = start || time;
+    
+    if($scope.matchRunning && time - start < MATCH_LENGTH) {
+      percentCompleted = 100*((time - start)/MATCH_LENGTH);
+      
+      //$apply allows for us to update the DOM as quickly as we need. Additional changes register quickly
+      $scope.$apply(function() {
+        //really an ng-style directive, but I only use it for this
+        $scope.barWidth = {'width':percentCompleted+"%"};
+        $scope.timeLeft = formatMilliseconds(MATCH_LENGTH - (time - start));
+
+        //sounds and color changes in bar
+        //for final 30 seconds
+        if($scope.timeLeft === '0:30') {
+          $scope.barColorClass = "progress-bar-warning";
+        }
+
+        //add
+        // end of autonomous (sound, color change)
+        // last 10 seconds (red)
+        // sound for last 30 sec.
+      });
+        
+      //if($scope.matchRunning) {
+        //recursive progress bar animation call
+        requestAnimationFrame($scope.bar);
+      //}
+    }
+
+    else {
+      //do server stuff - pass data to next page - etc.
+      //does not move to next page until pause is pressed - why?
+      $location.path('/verify');
+    }
+  };
 });
+
+app.controller('MasterMatchInputCtrl', function ($scope, $rootScope, $location) {
+  $scope.redTeams = [];
+  $scope.blueTeams = [];
+
+  $scope.createMatch = function() {
+    //returns a duplicate free array (a team can't play itself)
+    var uniqValArray = $scope.redTeams.concat($scope.blueTeams).uniq();
+    if(uniqValArray.length === 6 && validateTeams($scope.redTeams) && validateTeams($scope.blueTeams)) {
+      //sets rootScope currentTeams
+
+      //send socket.io event and wait for server validation.
+
+      //this is reactive... WHY???
+      $rootScope.currentTeams.red = $scope.redTeams;
+      $rootScope.currentTeams.blue = $scope.blueTeams;
+
+      $location.path('/master');
+    }
+    else {
+      throw new Error('teams do not work, please enter valid team ids');
+    }
+
+    //send request to server
+  };
+});
+
+// DOES NOT WORK. SCREW THIS FOR NOW. learn later.
+/*app.directive('progBar', function() {
+  return {
+    template: '<div class="progress-bar progress-bar-success" barWidthe:"width\: 10%;"></div>',
+    restrict: 'EA',
+    scope: {
+      countDown: '@'
+      startMatch: '&',
+      pauseMatch: '&',
+      resetMatch: '&'
+    },
+    //require: '^ngModel', //requires an ng-model to work
+    replace: true,
+    controller: function($scope) {},
+    link: function(scope, elements, attrs) {}
+  };
+});*/
+
 app.controller('MyCtrl5', function ($scope) {
   $scope.hello = 'test';
 
@@ -100,7 +259,6 @@ app.controller('MyCtrl8', function ($scope) {
   $scope.hello = 'test';
 
 });
-
 
 
 //Service, directive, and filter
