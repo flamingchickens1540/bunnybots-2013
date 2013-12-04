7ee06bb9acf8d2b6451bb8aad8439d351db81145
Hamzah Khan <hmzh.khn@gmail.com>
2013-12-03 16:11:34 -0800

    Numerous Changes
    Hook up socket.io (see README for event types)
    Integrate angular into many of the pages

--- app.js
+++ app.js
@@ -67,6 +67,73 @@ app.get('*', function(req, res){
   res.render('index');
 });
 
+
+/* SOCKET.IO EVENTS */
+io.sockets.on('connection', function(socket) {
+  console.log('connected to socket '+ socket.id);
+
+  /* MATCH */
+
+  //called from match input page for everything
+  //broadcasts match:init
+  //emits match:confirm-init
+  socket.on('match:init', function(data) {
+    // if correctly saves teams to database, then broadcast
+
+    db.createMatch(data, function(err) {
+      if(!err) {
+        socket.broadcast.emit('match:init', {red: data.red, blue: data.blue});
+        //allows master to move to macth page
+        socket.emit('match:confirm-init'); 
+      }
+      else 
+        console.error('error with saving initialized match and teams')
+    });
+    
+    //else return an error
+  });
+
+  //called by master match view for public match view
+  //broadcasts match:tick
+  socket.on('match:tick', function(data) {
+    //sends the match:tick event to all sockets except the current socket
+    socket.broadcast.emit('match:tick', data /*{percentCompleted: data.percentCompleted, timeLeft: data.timeLeft }*/);
+  });
+
+  //from master --> public views
+  //broadcasts match:end
+  socket.on('match:end', function(data) {
+    socket.broadcast.emit('match:end');
+  });
+
+  socket.on('match:reset', function() {
+    socket.broadcast.emit('match:reset');
+  });
+
+  //broadcasts match:recorded
+  socket.on('match:submit', function(data) {
+    db.finishMatch(data, function(err) {
+      if(!err) {
+        //allows master to switch back to input
+        //public sees final score
+        socket.broadcast.emit('match:recorded', {redScore: data.redAlliance.score, blueScore: data.blueAlliance.score});
+      }
+      else {
+        console.error('Match data not submitted and saved properly');
+      }
+    });
+  });
+
+  //realtime
+  //from referee --> match_views, other referees
+  //broadcasts referee:input
+  socket.on('referee:input', function(data) {
+    socket.broadcast.emit('referee:input', data);
+  });
+
+});
+
+
 /**
  * Start Server
  */
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,24 @@
-'use strict';
+"use strict";
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
+var helper = {};
+
 //returns an array of unique values
-Array.prototype.uniq = function(){
+helper.uniq = function uniq(array){
    var u = {}, a = [];
-   for(var i = 0, l = this.length; i < l; ++i){
-      if(u.hasOwnProperty(this[i])) {
+   for(var i = 0, l = array.length; i < l; ++i){
+      if(u.hasOwnProperty(array[i])) {
          continue;
       }
-      a.push(this[i]);
-      u[this[i]] = 1;
+      a.push(array[i]);
+      u[array[i]] = 1;
    }
    return a;
 }
 
 //check that it is made of 3 teams, each 4-5 letters long and strings
-var validateTeams = function validateTeams(teamsArray) {
+helper.validateTeams = function validateTeams(teamsArray) {
   if(angular.isArray(teamsArray)) {
     var validateAllianceCount = (teamsArray.length === 3); 
     var validateTypes, validateWordLengths;
@@ -26,7 +28,7 @@ var validateTeams = function validateTeams(teamsArray) {
         validateTypes = true;
 
         // '1540' or '1540z'
-        if(team.length <= 5 && team.length >= 1) {
+        if(team.length <= 5 && team.length > 0) {
           validateWordLengths = true;
         }
         else {
@@ -45,7 +47,7 @@ var validateTeams = function validateTeams(teamsArray) {
   }
 };
 
-var formatMilliseconds = function formatMillisceonds(time) {
+helper.formatMilliseconds = function formatMillisceonds(time) {
   if(typeof time === 'string') {
     time = parseInt(time);
   }
@@ -62,9 +64,8 @@ var formatMilliseconds = function formatMillisceonds(time) {
 
 var app = angular.module('bunnybots2013', ['ngRoute', 'btford.socket-io']);
 
-app.config(function ($routeProvider, $locationProvider) {
 
-app.config(function ($routeProvider, $locationProvider, socketProvider) {
+app.config(function ($routeProvider, $locationProvider) {
   $routeProvider.
 
     when('/', {
@@ -116,15 +117,97 @@ app.config(function ($routeProvider, $locationProvider, socketProvider) {
 
 app.controller('AppCtrl', function ($scope, $rootScope, socket) {
   //update by master only
-  $rootScope.currentTeams = {red:[],blue:[]};
 
+  //try not to use rootScope
+  $rootScope.currentTeams = {red:[],blue:[]};
+  $rootScope.redAlliance = {};
+  $rootScope.blueAlliance = {};
+
+  socket.on('connected', function(data) {
+    console.log('LOG: socket.io connected to server.');
+  });
 });
 app.controller('HomePageCtrl', function ($scope) {
   
   $scope.hello = 'test';
 });
-app.controller('PublicMatchViewCtrl', function ($scope, $rootScope, socket) {
 
+app.controller('PublicMatchViewCtrl', function ($scope, $rootScope, socket) {
+  $scope.redTeams = $rootScope.currentTeams.red;
+  $scope.blueTeams = $rootScope.currentTeams.blue;
+
+  $scope.redFouls = 0;
+  $scope.blueFouls = 0;
+
+  socket.on('match:reset', function() {
+    $scope.redScore = 0;
+    $scope.blueScore = 0;
+    $scope.timeLeft = '2:30';
+    $scope.barWidth = {width: '0%'};
+  });
+
+  socket.on('match:init', function(data) {
+    //assign redTeams and blueTeams
+    $scope.redTeams = data.red;
+    $scope.blueTeams = data.blue;
+    $scope.barWidth = {width: '0%'};
+    $scope.timeLeft = '2:30';
+    $scope.redScore = 0;
+    $scope.blueScore = 0;
+    //reset timer
+  });
+
+  //may need to update for lag and latency
+  socket.on('match:tick', function(data) {
+    $scope.$apply(function() {
+        
+
+      //really an ng-style directive, but I only use it for this
+      $scope.barWidth = {'width':data.percentCompleted+"%"};
+      $scope.timeLeft = data.timeLeft;
+
+      //sounds and color changes in bar
+      //for final 30 seconds
+      if($scope.timeLeft === '0:30') {
+        $scope.barColorClass = "progress-bar-warning";
+      }
+
+      //add
+      // end of autonomous (sound, color change)
+      // last 10 seconds (red)
+      // sound for last 30 sec.
+
+      //fixes issue of bar not completing
+      if($scope.timeLeft === '0:00') {
+        $scope.barWidth = {width: '100%'};
+      }
+    });
+  });
+
+  socket.on('referee:input', function(data) {
+
+    //should only be one key value pair per signal
+    var thingToChange = Object.keys(data);
+    $scope[thingToChange] += data[thingToChange];
+
+    if(thingToChange === 'redFouls') {
+      $scope.redScore += 3*data.redFouls;
+    }
+
+    else if(thingToChange === 'blueFouls') {
+      $scope.blueScore += 3*data.blueFouls;
+    }
+  });
+
+  socket.on('match:end', function() {
+    $scope.timeLeft = 'Verifying...'
+  });
+
+  socket.on('match:recorded', function(data) {
+    $scope.timeLeft = 'Complete';
+    $scope.redScore = data.redScore;
+    $scope.blueScore = data.blueScore;
+  });
 });
 
 app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, socket) {
@@ -132,10 +215,24 @@ app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, s
   $scope.redTeams = $rootScope.currentTeams.red;
   $scope.blueTeams = $rootScope.currentTeams.blue;
 
+  $scope.redFouls = 0;
+  $scope.blueFouls = 0;
+
   $scope.startMatch = function() {
     if(!$scope.matchRunning) {
       requestAnimationFrame($scope.bar);
     }
+  };
+  $scope.verifyMatch = function() {
+    socket.emit('match:end');
+
+    $rootScope.redAlliance = {score: $scope.redScore, fouls: $scope.redFouls};
+    $rootScope.blueAlliance = {score: $scope.blueScore, fouls: $scope.blueFouls};
+
+    $location.path('/verify');
+  };
+  $scope.resetMatch = function() {
+    socket.emit('match:reset');
   };
   /*
     reset: coded for in HTML
@@ -147,7 +244,7 @@ app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, s
   var MATCH_LENGTH = 150000;
 
   var start;
-  var percentCompleted = 0;
+  var percentCompleted = 0, timeLeft = '2:30';
   $scope.barColorClass = "progress-bar-success";
 
   $scope.bar = function bar(time) {
@@ -158,9 +255,19 @@ app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, s
       
       //$apply allows for us to update the DOM as quickly as we need. Additional changes register quickly
       $scope.$apply(function() {
+        
+
         //really an ng-style directive, but I only use it for this
         $scope.barWidth = {'width':percentCompleted+"%"};
-        $scope.timeLeft = formatMilliseconds(MATCH_LENGTH - (time - start));
+        $scope.timeLeft = helper.formatMilliseconds(MATCH_LENGTH - (time - start));
+
+        if(timeLeft !== $scope.timeLeft) {
+          timeLeft = $scope.timeLeft;
+
+          //send match:tick event
+          socket.emit('match:tick', {timeLeft: timeLeft, percentCompleted: percentCompleted});
+          //console.log('emit match:tick event at '+ timeLeft);
+        }
 
         //sounds and color changes in bar
         //for final 30 seconds
@@ -172,6 +279,12 @@ app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, s
         // end of autonomous (sound, color change)
         // last 10 seconds (red)
         // sound for last 30 sec.
+
+
+        if($scope.timeLeft === '0:00') {
+          //end match starts the verify button
+          $scope.matchComplete = true;
+        }
       });
         
       //if($scope.matchRunning) {
@@ -183,9 +296,25 @@ app.controller('MasterMatchViewCtrl', function ($scope, $rootScope, $location, s
     else {
       //do server stuff - pass data to next page - etc.
       //does not move to next page until pause is pressed - why?
-      $location.path('/verify');
+      //$location.path('/verify');
     }
   };
+
+  socket.on('referee:input', function(data) {
+    //should only be one key value pair per signal
+    //first element in array returned is my value
+    var thingToChange = Object.keys(data)[0];
+
+    $scope[thingToChange] += data[thingToChange];
+
+    if(thingToChange === 'redFouls') {
+      $scope.blueScore += 3*data.redFouls;
+    }
+
+    else if(thingToChange === 'blueFouls') {
+      $scope.redScore += 3*data.blueFouls;
+    }
+  });
 });
 
 app.controller('MasterMatchInputCtrl', function ($scope, $rootScope, $location, socket) {
@@ -194,31 +323,163 @@ app.controller('MasterMatchInputCtrl', function ($scope, $rootScope, $location, 
 
   $scope.createMatch = function() {
     //returns a duplicate free array (a team can't play itself)
-    var uniqValArray = $scope.redTeams.concat($scope.blueTeams).uniq();
-    if(uniqValArray.length === 6 && validateTeams($scope.redTeams) && validateTeams($scope.blueTeams)) {
-      //sets rootScope currentTeams
+    var uniqValArray = helper.uniq($scope.redTeams.concat($scope.blueTeams));
+    if(uniqValArray.length === 6 && helper.validateTeams($scope.redTeams) && helper.validateTeams($scope.blueTeams)) {
 
       //send socket.io event and wait for server validation.
-
-      //this is reactive... WHY???
-      $rootScope.currentTeams.red = $scope.redTeams;
-      $rootScope.currentTeams.blue = $scope.blueTeams;
-
-      $location.path('/master');
+      socket.emit('match:init', {red: $scope.redTeams, blue: $scope.blueTeams});
     }
     else {
-      throw new Error('teams do not work, please enter valid team ids');
+      console.error('teams do not work, please enter valid team ids');
     }
-
-    //send request to server
   };
+
+  socket.on('match:confirm-init', function(data) {
+    //reactive equaling
+    $rootScope.currentTeams.red = $scope.redTeams;
+    $rootScope.currentTeams.blue = $scope.blueTeams;
+    $location.path('/master');
+  });
+
+  socket.on('error-init', function(err) {
+    alert(err.message);
+    console.error(err.message);
+  });
 });
 
 app.controller('MasterMatchVerifyCtrl', function ($scope, $rootScope, socket, $location) {
+  //allow for easy editing of flawed scores
+  $scope.redTeams = $rootScope.currentTeams.red;
+  $scope.blueTeams = $rootScope.currentTeams.blue;
+
+  $scope.redScore = $rootScope.redAlliance.score || 0;
+  $scope.redFouls = $rootScope.redAlliance.fouls || 0;
+
+  $scope.blueScore = $rootScope.blueAlliance.score || 0;
+  $scope.blueFouls = $rootScope.blueAlliance.fouls || 0;
+
+  $scope.editStats = function editStats(color, type, num) {
+    if((color === 'red' || color === 'blue') && (type === 'fouls' || type === 'score') && typeof num === 'number') {
+      var formattedType = type[0].toUpperCase() + type.slice(1)
+      $scope[color+formattedType] += num;
+
+      if(type === 'fouls') {
+        var otherColor = (color === 'red')? 'blue': 'red';
+        $scope[otherColor+'Score'] += 3*num;
+      }
+
+      return true;
+    }
+    else {
+      throw new Error("can't edit the score");
+      return false;
+    }
+  };
+
+  // NO err-checking!
+  $scope.submitMatch = function() {
+    //send matchResults to server
+    //then, show final score page for user
+
+    socket.emit('match:submit', {
+      redAlliance: {
+        teams: $scope.redTeams,
+        score: $scope.redScore,
+        fouls: $scope.redFouls
+        },
+      blueAlliance: {
+        teams: $scope.blueTeams,
+        score: $scope.blueScore,
+        fouls: $scope.blueFouls
+      }
+    });
+  };
+
+  socket.on('match:recorded', function() {
+    $location.path('/input');
+  });
 });
 
-app.controller('RefereeCtrl', function ($scope) {
-  $scope.hello = 'test';
+app.controller('RefereeCtrl', function ($scope, socket) {
+
+  //default is disabled or enabled? which should I choose?
+
+  var reset = function() {
+    //enable all buttons
+    $scope.redFouls = 0;
+    $scope.redScore = 0;
+    $scope.blueFouls = 0;
+    $scope.blueScore = 0;
+    //reset hole color
+    $scope.timeLeft = '2:30';
+  };
+
+
+  var editStats = function editStats(color, type, num) {
+    if((color === 'red' || color === 'blue') && (type === 'fouls' || type === 'score') && typeof num === 'number') {
+      var formattedType = type[0].toUpperCase() + type.slice(1);
+      $scope[color+formattedType] += num;
+
+      if(type === 'fouls') {
+        var otherColor = (color === 'red')? 'blue': 'red';
+        $scope[otherColor+'Score'] += 3*num;
+      }
+
+      return true;
+    }
+    else {
+      throw new Error("can't edit the score");
+      return false;
+    }
+  };
+
+  $scope.emitRefereeInput = function(color, type, num) {
+    var editedStats = editStats(color, type, num);
+    if(editedStats) {
+      var camelCased = color + type[0].toUpperCase() + type.slice(1)
+
+      var dataObj = {};
+      dataObj[camelCased] = num;
+
+      socket.emit('referee:input', dataObj);
+    }
+  };
+
+  $scope.changeHoleColor = function(newColor) {
+
+  };
+
+  socket.on('match:init', reset);
+  socket.on('match:reset', reset);
+
+  socket.on('match:tick', function(data) {
+    $scope.timeLeft = data.timeLeft;
+  });
+
+  socket.on('referee:input', function(data) {
+
+    //should only be one key value pair per signal
+    var thingToChange = Object.keys(data);
+    $scope[thingToChange] += data[thingToChange];
+
+    if(thingToChange === 'redFouls') {
+      $scope.redScore += 3*data.redFouls;
+    }
+
+    else if(thingToChange === 'blueFouls') {
+      $scope.blueScore += 3*data.blueFouls;
+    }
+  });
+
+  socket.on('match:end', function(data) {
+    //show message in timeLeft
+    //disable buttons
+  });
+
+  socket.on('match:recorded', function(data) {
+    //timeLeft = waiting for next match
+  });
+
 
 });
 app.controller('TeamRankingsCtrl', function ($scope) {
